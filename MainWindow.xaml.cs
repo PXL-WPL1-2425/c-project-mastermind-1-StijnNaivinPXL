8a2f130f5fd9155edb4e352759399cfeb323f965
Stijn Naivin <stijn.naivin@student.pxl.be>
2024-11-11 16:45:51 +0100

    Mastermind - 02
    Random kleuren in de vorm van indexen en een enumerator om een goede leesbaarheid te behouden met minimale performantieverlies. Samen met een property met automatische getter om een string-representatie van de kleuren te krijgen.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,42 @@ namespace MasterMind
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Array, less memory allocation
+        private Colors[] colors = new Colors[4];
+
+        private Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            SetupGame();
         }
+
+        private void SetupGame()
+        {
+            for (int i = 0; i < colors.Length; i++)
+            {
+                //Typecasting int => enum:int
+                colors[i] = (Colors)random.Next(0, colors.Length);
+            }
+            UpdateTiteName();
+        }
+
+        private void UpdateTiteName()
+        {
+            this.Title = $"Mastermind - {ColorString}";
+        }
+
+        private string ColorString => $"({string.Join(", ", colors)})";
+    }
+
+    //Inside a namespace, easier access.
+    enum Colors : int
+    {
+        Blue,
+        Red,
+        Orange,
+        Yellow,
+        White,
     }
 }
