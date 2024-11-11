d8e737ec9d8b23eeef4a099c9f5c7ade1a9a4ed3
Stijn Naivin <stijn.naivin@student.pxl.be>
2024-11-11 16:58:51 +0100

    Mastermind - 03
    De kleuren worden ingeladen via de enumerator.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,24 +17,49 @@ namespace MasterMind
     public partial class MainWindow : Window
     {
         //Array, less memory allocation
-        private Colors[] colors = new Colors[4];
+        private TypeColors[] colors = new TypeColors[4];
 
         private Random random = new Random();
 
         public MainWindow()
         {
             InitializeComponent();
+            // Ik hoop dat ik dit mag doen, ik verkies dit boven verbinden in XAML.
+            //CheckCodeBtn.Click += OnClickCheckCodeBtn;
             SetupGame();
         }
+
+        /*private void OnClickCheckCodeBtn(object sender, RoutedEventArgs e)
+        {
+
+        }*/
 
         private void SetupGame()
         {
             for (int i = 0; i < colors.Length; i++)
             {
                 //Typecasting int => enum:int
-                colors[i] = (Colors)random.Next(0, colors.Length);
+                colors[i] = (TypeColors)random.Next(0, colors.Length);
             }
             UpdateTiteName();
+            FillComboBoxes();
+        }
+
+        private void FillComboBoxes()
+        {
+            Color1Cb.Items.Clear();
+            Color2Cb.Items.Clear();
+            Color3Cb.Items.Clear();
+            Color4Cb.Items.Clear();
+
+            // Heel erg hacky, maar ik verkies wat meer leesbaarheid over strings te gebruiken.
+            foreach (TypeColors color in (TypeColors[])Enum.GetValues(typeof(TypeColors)))
+            {
+                Color1Cb.Items.Add(color);
+                Color2Cb.Items.Add(color);
+                Color3Cb.Items.Add(color);
+                Color4Cb.Items.Add(color);
+            }
         }
 
         private void UpdateTiteName()
@@ -45,12 +70,15 @@ namespace MasterMind
         private string ColorString => $"({string.Join(", ", colors)})";
     }
 
-    //Inside a namespace, easier access.
-    enum Colors : int
+    // Inside a namespace, easier access.
+    // Kleur - 03 Rename
+    enum TypeColors : int
     {
         Blue,
         Red,
         Orange,
+        // Kleur - 03 patch
+        Green,
         Yellow,
         White,
     }
